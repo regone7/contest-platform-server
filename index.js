d7262410419de2d5dc5657d25a1b60481abf42e5
regone7 <regone12345@gmail.com>
2024-06-03 23:42:47 +0600

    admin handel server

--- index.js
+++ index.js
@@ -19,36 +19,47 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4zosjqm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
 
-    const userCollection = client.db("contestDB").collection("users");
+        const userCollection = client.db("contestDB").collection("users");
 
-    // Send a ping to confirm a successful connection
+        // Send a ping to confirm a successful connection
 
-    app.post('/users', async (req, res) => {
-        const infouser = req.body;
-        console.log()
-        const result = await userCollection.insertOne(infouser);
-        res.send(result)
+        app.post('/users', async (req, res) => {
+            const infouser = req.body;
+            console.log()
+            const query = { email: infouser.email }
+            const existingUser = await userCollection.findOne(query);
+            if (existingUser) {
+                return res.send({ message: 'user already exists', insertedId: null })
+            }
+            const result = await userCollection.insertOne(infouser);
+            res.send(result)
 
-    })
+        })
+        app.get('/user/:email', async (req, res) => {
+            const email = req.params.email
+            console.log(email)
+            const result = await userCollection.findOne({ email })
+            res.send(result)
+        })
 
 
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
